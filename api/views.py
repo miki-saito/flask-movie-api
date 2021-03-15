ba9428b84ae4f5fb6a0728a8aff61cc1f5e3904c
miki-saito <65391589+miki-saito@users.noreply.github.com>
2021-03-15 18:15:19 +0900

    2021-03-15

--- api/views.py
+++ api/views.py
@@ -1,26 +1,26 @@
 from flask import Blueprint, jsonify, request
-from . import db 
-from .models import Movie
+# from . import db 
+# from .models import Movie
 
 main = Blueprint('main', __name__)
 
 @main.route('/add_movie', methods=['POST'])
 def add_movie():
-    movie_data = request.get_json()
+    # movie_data = request.get_json()
 
-    new_movie = Movie(title=movie_data['title'], rating=movie_data['rating'])
+    # new_movie = Movie(title=movie_data['title'], rating=movie_data['rating'])
 
-    db.session.add(new_movie)
-    db.session.commit()
+    # db.session.add(new_movie)
+    # db.session.commit()
 
     return 'Done', 201
 
 @main.route('/movies')
 def movies():
-    movie_list = Movie.query.all()
+    # movie_list = Movie.query.all()
     movies = []
 
-    for movie in movie_list:
-        movies.append({'title' : movie.title, 'rating' : movie.rating})
+    # for movie in movie_list:
+        # movies.append({'title' : movie.title, 'rating' : movie.rating})
 
     return jsonify({'movies' : movies})
