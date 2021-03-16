69586ce5970faf1405a44411944a8561f3650be7
miki-saito <65391589+miki-saito@users.noreply.github.com>
2021-03-16 11:35:07 +0900

    2021-03-16-1

--- api/models.py
+++ api/models.py
@@ -1,6 +1,6 @@
 from . import db 
 
 class Movie(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(50))
-    rating = db.Column(db.Integer)
+    id = db.Column(db.Integer, primary_key=True) #整数値、主キー指定
+    title = db.Column(db.String(50)) #50文字の長さの文字列
+    rating = db.Column(db.Integer) # 整数値
