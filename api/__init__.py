37fe0d5b1a8d3e56e9d82bce9f12e1e466da8403
miki-saito <65391589+miki-saito@users.noreply.github.com>
2021-03-16 11:40:36 +0900

    2021-03-16-2

--- api/__init__.py
+++ api/__init__.py
@@ -1,14 +1,14 @@
 from flask import Flask 
 from flask_sqlalchemy import SQLAlchemy
 
-# db = SQLAlchemy()
+db = SQLAlchemy() # SQLAlchemyをインスタンス化し、何も引数に渡さない
 
 def create_app():
     app = Flask(__name__)
 
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
-    # db.init_app(app)
+    db.init_app(app) # DBオブジェクトを作成し、init_app()を使用し、appオブジェクトを渡す。
 
     from .views import main
     app.register_blueprint(main)
